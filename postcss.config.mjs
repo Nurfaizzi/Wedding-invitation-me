9e6cf63f559999198621fad6ce42fa4d7fea3c4f
Nurfaizzi <ocitbmc@gmail.com>
2025-11-24 12:55:04 +0700

    fixxx

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,15 +2,6 @@ const config = {
   plugins: ["@tailwindcss/postcss"],
 
 
-    screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
-      // Add your custom screens here
-    
-  }
 };
 
 export default config;
